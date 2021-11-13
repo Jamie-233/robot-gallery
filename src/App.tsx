0c64cd3037cf7c398b6fe5f4b8752a3ba1668e8f
Jenkin <jiancunge.com@gmail.com>
2021-11-13 19:10:24 +0800

    feat: adds robot shopping cart compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
 import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
+import ShoppingCard from './components/ShoppingCart';
 import styles from './App.module.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1>Robot Online</h1>
+      </div>
+      <ShoppingCard />
       <div className={styles.robotList}>
         {robots.map((r) => (
-          <Robot id={r.id} name={r.name} email={r.email} />
+          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
         ))}
       </div>
     </div>
