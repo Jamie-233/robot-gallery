2614cf91c4d63b2af60b10ee6d43052ef9c81997
Jenkin <jiancunge.com@gmail.com>
2021-11-14 17:06:22 +0800

    feat: adds get users api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import robots from './mockdata/robots.json';
+import React from 'react';
+// import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
-import ShoppingCard from './components/ShoppingCart';
+import ShoppingCart from './components/ShoppingCart';
 import styles from './App.module.css';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="logo" className={styles.appLogo} />
-        <h1>Robot Online</h1>
+interface Props {}
+
+interface State {
+  robotGallery: any[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      robotGallery: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => this.setState({ robotGallery: data }));
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="logo" className={styles.appLogo} />
+          <h1>Robot Online</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => (
+            <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
+          ))}
+        </div>
       </div>
-      <ShoppingCard />
-      <div className={styles.robotList}>
-        {robots.map((r) => (
-          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
