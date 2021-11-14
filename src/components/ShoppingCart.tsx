bc93c2d03241f049a268f875abeed4142fce623b
Jenkin <jiancunge.com@gmail.com>
2021-11-14 16:20:49 +0800

    feat: adds icons

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './ShoppingCart.module.css';
+import { FiShoppingCart } from 'react-icons/fi';
 
 interface Props {}
 
 interface State {
-  isOpen: boolean; // menu stat flag
+  isOpen: boolean;
 }
 
 class ShoppingCard extends React.Component<Props, State> {
@@ -15,11 +16,26 @@ class ShoppingCard extends React.Component<Props, State> {
     };
   }
 
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if ((e.target as HTMLElement).nodeName === 'SPAN') {
+      this.setState({
+        isOpen: !this.state.isOpen,
+      });
+    }
+  };
+
   render() {
     return (
       <div className={styles.cartContainer}>
-        <button className={styles.button}>购物车 2(件)</button>
-        <div className={styles.cartDropDown}>
+        <button className={styles.button} onClick={this.handleClick}>
+          <FiShoppingCart />
+          <span>购物车 2(件)</span>
+        </button>
+        <div
+          className={styles.cartDropDown}
+          style={{
+            display: this.state.isOpen ? 'block' : 'none',
+          }}>
           <ul>
             <li>robot 1</li>
             <li>robot 2</li>
